b04eccf3e8b619867c09077e9c7aef889aefea79
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-03-30 16:06:51 -0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import "./index.css";
-import App from "./App";
-import ProductsProvider from "./context/products-context";
+import './index.css';
+import App from './App';
+import configureProductsStore from './hooks-store/products-store';
+import configureCounterStore from './hooks-store/counter-store';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>
+configureProductsStore();
+configureCounterStore();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
