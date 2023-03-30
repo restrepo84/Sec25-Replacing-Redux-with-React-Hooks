b67aa4ceba271411cc458fa7c44dc1d6efe8ff68
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-03-30 14:57:10 -0600

    toggling fav with the context API

---
+++ src/store/reducers/products.js
@@ -0,0 +1,24 @@
+// import { TOGGLE_FAV } from "../actions/products";
+
+// const productReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case TOGGLE_FAV:
+//       const prodIndex = state.products.findIndex(
+//         (p) => p.id === action.productId
+//       );
+//       const newFavStatus = !state.products[prodIndex].isFavorite;
+//       const updatedProducts = [...state.products];
+//       updatedProducts[prodIndex] = {
+//         ...state.products[prodIndex],
+//         isFavorite: newFavStatus,
+//       };
+//       return {
+//         ...state,
+//         products: updatedProducts,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default productReducer;
